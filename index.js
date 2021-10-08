1b7ba979e42df5bc8e79e98bc89bb91204907c36
Carlos Mejia <papacarlos13@gmail.com>
2021-10-07 21:16:12 -0500

    fix hamburger

--- index.js
+++ index.js
@@ -23,6 +23,10 @@ function render(st) {
   ${Footer()}
   `;
   router.updatePageLinks();
+
+  document.querySelector(".fa-bars").addEventListener("click", () => {
+    document.querySelector("nav > ul").classList.toggle("hidden--mobile");
+  });
 }
 
 document
@@ -33,7 +37,3 @@ document
       (event.target.title =
         "The Groundhog is ready to help you forecast the next 6 weeks!")
   );
-
-document.querySelector(".fa-bars").addEventListener("click", () => {
-  document.querySelector("nav > ul").classList.toggle("hidden--mobile");
-});
